7cc010fcd47f89a16f1a3cb372b2b770f419ba56
hogreed <pushan45hsh@gmail.com>
2021-10-08 10:25:56 +0900

    중간 저장

--- contract/exemple/featuredMarket/IERC721Standard.sol
+++ contract/exemple/featuredMarket/IERC721Standard.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
 
-import "./IERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
 
 
 /**
